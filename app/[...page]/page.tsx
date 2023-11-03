65f1f3ddff53abf023d74a82f585e44e1a33e9f0
Christiyan <christiyanweb@gmail.com>
2023-11-03 14:12:36 +0200

    [feat] Update page

--- app/[...page]/page.tsx
+++ app/[...page]/page.tsx
@@ -13,7 +13,7 @@ interface PageProps {
 export default async function Page(props: PageProps) {
   const content = await builder
     // Get the page content from Builder with the specified options
-    .get("page", {
+    .get("figma-imports", {
       userAttributes: {
         // Use the page path specified in the URL to fetch the content
         urlPath: "/" + (props?.params?.page?.join("/") || ""),
